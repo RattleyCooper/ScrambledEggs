d2bc544f8d8d2ecbb69ff40eddf26eb24430b355
py-am-i <parkeryoung228@gmail.com>
2018-01-15 10:08:19 -0800

    Scrambler update, see details
    Scrambler encodes the first letter where before it left it as the same
    character so the unscramble method could decode everything.  Now the
    scramble/unscramble method can handle an encoded first character.
    Moved functions to `common.py` and created `Dial` object to handle
    figuring out which characters to pick when encoding.  It’s a little bit
    slower than pure math, however it should never have to “count” more
    than the higher end of the given `rng`.

--- __init__.py
+++ __init__.py
@@ -1,70 +1,77 @@
 import string as st
-
-
-def circular_add(start, amount, rng=(32, 126)):
-    output = start + amount
-    if output > rng[1]:
-        output -= rng[1]
-        output += rng[0] - 1
-        if output > rng[1]:
-            output = circular_add(rng[1], output - rng[1], rng=rng)
-
-    return output
-
-
-def circular_subtract(start, amount, rng=(32, 126)):
-    output = start - amount
-
-    if output < rng[0]:
-        output += rng[1] + 1
-        output -= rng[0]
-        if output < rng[0]:
-            output = circular_subtract(rng[0], rng[0] - output, rng=rng)
-
-    return output
+from common import Dial
 
 
 class Scrambler(object):
-    characters = [c for c in st.ascii_letters + "1234567890-=`~!@#$%^&*()_+\\][|{}';/.,\":?>< "]
+    # characters = [c for c in st.ascii_letters + "1234567890-=`~!@#$%^&*()_+\\][|{}';/.,\":?>< "]
+    characters = [c for c in st.printable]
     rev_ord = {ord(c): c for c in characters}
     ord = {c: ord(c) for c in characters}
 
+    def __init__(self):
+        Scrambler.rev_ord[31] = '\n'
+        Scrambler.ord['\n'] = 31
+
+        Scrambler.rev_ord[30] = '\t'
+        Scrambler.ord['\t'] = 30
+
     def scramble(self, string):
-        salt = Scrambler.ord[string[0]]
+        if not string:
+            raise ValueError("String cannot be empty.")
+
+        first_letter_ord = Scrambler.ord[string[0]]
         output = ""
 
         for i, c in enumerate(string):
-            c_ord = Scrambler.ord[c]
+            d1 = Dial(Scrambler.ord[c])
+            c_ord = d1.add(sum([Scrambler.ord[x] for x in string[:i]]))
             if i == 0:
                 out = c
             else:
-                out = Scrambler.rev_ord[circular_add(salt, c_ord)]
+                d1.set_pointer(first_letter_ord)
+                _ = d1.add(c_ord)
+                out = Scrambler.rev_ord[_]
 
             output += out
-            salt = Scrambler.ord[c]
+            first_letter_ord = Scrambler.ord[c]
 
+        d1.set_pointer(Scrambler.ord[string[0]])
+        first_letter_number = d1.add(sum([Scrambler.ord[x] for x in output[1:]]))
+        output = Scrambler.rev_ord[first_letter_number] + output[1:]
         return output
 
     def unscramble(self, string):
-        salt = Scrambler.ord[string[0]]
-        if not salt:
+        if not string:
+            raise ValueError("String cannot be empty.")
+
+        first_letter_ord = Scrambler.ord[string[0]]
+        if not first_letter_ord:
             raise ValueError("Your message is invalid.")
 
         output = ""
 
+        # Set string's first letter to the correct un-encoded letter.
+        d1 = Dial(first_letter_ord)
+        first_letter_number = d1.sub(sum([Scrambler.ord[x] for x in string[1:]]))
+        first_letter = Scrambler.rev_ord[first_letter_number]
+        string = first_letter + string[1:]
+
         for i, c in enumerate(string):
-            c_ord = Scrambler.ord[c]
+            d1.set_pointer(Scrambler.ord[c])
+            c_ord = d1.sub(sum([Scrambler.ord[x] for x in output[:i]]))
             if i == 0:
                 out = c
             else:
                 last_c_ord = Scrambler.ord[output[-1]]
-                out_ord = circular_subtract(c_ord, last_c_ord)
+                d1.set_pointer(c_ord)
+                out_ord = d1.sub(last_c_ord)
                 out = Scrambler.rev_ord[out_ord]
 
             output += out
-            salt = Scrambler.ord[out]
+            first_letter_ord = Scrambler.ord[out]
 
         return output
+
 
 print(''.join(Scrambler.characters))
 d = """Paul awoke to feel himself in the warmth of his bed - thinking... thinking. This world of Castle Caladan, without play or companions his own age, perhaps did not deserve sadness in farewell. Dr. Yueh, his teacher, had hinted that the faufreluches class system was not rigidly guarded on Arrakis. The planet sheltered people who lived at the desert edge without caid or bashar to command them: will-o'-the-sand people called Fremen, marked down on no census of the Imperial Regate.s"""
@@ -73,8 +80,82 @@ d3 = """They've been pumping basements around the clock in neighborhoods like Pi
 d4 = "Scrambled eggs are yummy."
 
 s = Scrambler()
-o = s.scramble(d4)
-print(o)
-m = s.unscramble(o)
-print(m)
 
+print()
+o = "Going down to south park\ngonna have myself a time."
+print('string:', o)
+o = s.scramble(o)
+print('\nencoded:', o)
+m = s.unscramble(o)
+print('\ndecoded:', m)
+
+o = """import string as st
+from common import circular_add, circular_subtract
+
+
+class Scrambler(object):
+    # characters = [c for c in st.ascii_letters + "1234567890-=`~!@#$%^&*()_+\\][|{}';/.,\":?>< "]
+    characters = [c for c in st.printable]
+    rev_ord = {ord(c): c for c in characters}
+    ord = {c: ord(c) for c in characters}
+
+    def __init__(self):
+        Scrambler.rev_ord[31] = '\n'
+        Scrambler.ord['\n'] = 31
+
+        Scrambler.rev_ord[30] = '\t'
+        Scrambler.ord['\t'] = 30
+
+    def scramble(self, string):
+        first_letter_ord = Scrambler.ord[string[0]]
+        output = ""
+
+        for i, c in enumerate(string):
+            c_ord = circular_add(Scrambler.ord[c], sum([Scrambler.ord[x] for x in string[:i]]))
+            if i == 0:
+                out = c
+            else:
+                _ = circular_add(first_letter_ord, c_ord)
+                out = Scrambler.rev_ord[_]
+
+            output += out
+            first_letter_ord = Scrambler.ord[c]
+
+        # Set first character to an encoded character based on the sum of all characters after the first character.
+        first_letter_number = circular_add(Scrambler.ord[string[0]], sum([Scrambler.ord[x] for x in output[1:]]))
+        output = Scrambler.rev_ord[first_letter_number] + output[1:]
+        return output
+
+    def unscramble(self, string):
+        first_letter_ord = Scrambler.ord[string[0]]
+        if not first_letter_ord:
+            raise ValueError("Your message is invalid.")
+
+        output = ""
+
+        # Set string's first letter to the correct un-encoded letter.
+        first_letter_number = circular_subtract(first_letter_ord, sum([Scrambler.ord[x] for x in string[1:]]))
+        first_letter = Scrambler.rev_ord[first_letter_number]
+        string = first_letter + string[1:]
+
+        for i, c in enumerate(string):
+            c_ord = circular_subtract(Scrambler.ord[c], sum([Scrambler.ord[x] for x in output[:i]]))
+            # c_ord = Scrambler.ord[c]
+            if i == 0:
+                out = c
+            else:
+                last_c_ord = Scrambler.ord[output[-1]]
+                out_ord = circular_subtract(c_ord, last_c_ord)
+                out = Scrambler.rev_ord[out_ord]
+
+            output += out
+            first_letter_ord = Scrambler.ord[out]
+
+        return output
+"""
+
+print('\nstring:', o)
+o = s.scramble(o)
+print('\nencoded:', o)
+o = s.unscramble(o)
+print('\ndecoded:', o)
--- common.py
+++ common.py
@@ -23,28 +23,66 @@ def circular_subtract(start, amount, rng=(30, 126)):
     return output
 
 
-def dial_add(start, amount, rng=(30, 126)):
-    o = start + amount
-    if o > rng[1]:
-        return o % rng[1] + (rng[0] - 1)
+class Dial(object):
+    def __init__(self, start, rng=(30, 126)):
+        self.start = start
+        self.rng = rng
+        self.full_rng = rng[1] - rng[0]
+        self.rng_list = list(range(rng[0], rng[1] + 1))
+        self.pointer = self.rng_list.index(start)
+        self.current = self.rng_list[self.pointer]
 
-    return o
+    def _increment(self):
+        if self.pointer == len(self.rng_list) - 1:
+            self.pointer = 0
+            self.current = self.rng_list[self.pointer]
+            return True
+        self.pointer += 1
+        self.current = self.rng_list[self.pointer]
+        return True
+
+    def _decrement(self):
+        if self.pointer == 0:
+            self.pointer = len(self.rng_list) - 1
+            self.current = self.rng_list[self.pointer]
+            return True
+
+        self.pointer -= 1
+        self.current = self.rng_list[self.pointer]
+        return True
+
+    def set_pointer(self, point):
+        self.pointer = self.rng_list.index(point)
+        self.current = self.rng_list[self.pointer]
+        return self
+
+    def add(self, amount):
+        while amount > self.rng[1]:
+            amount = amount // self.full_rng + amount % self.full_rng
+
+        for i in range(amount):
+            self._increment()
+
+        return self.current
+
+    def sub(self, amount):
+        # Reduce the amount of iterations we need to perform.
+        while amount > self.rng[1]:
+            amount = amount // self.full_rng + amount % self.full_rng
+
+        for i in range(amount):
+            self._decrement()
+
+        return self.current
 
 
-def dial_sub(start, amount, rng=(30, 126)):
-    output = start - amount
+# d = Dial(90)
+# print(d.add(17334))
+# print(d.add(5))
+# print(d.sub(17334))
+# print()
+# print(circular_add(90, 17334, rng=(30, 126)))
+# print(circular_subtract(90, 17334, rng=(30, 126)))
 
-    # Handle negative numbers.
-    if output < 0:
-        subtract_amount = abs(output) + 32
-        output = (rng[1] - subtract_amount) + 1
-        if output < rng[0]:
-            output = dial_sub(rng[0], rng[0] - output, rng=rng)
-
-    # Handle outputs less than the lower end of the range.
-    if output < rng[0]:
-        output = rng[1] - (rng[0] % (start - amount)) + 1
-
-    return output
 
 
